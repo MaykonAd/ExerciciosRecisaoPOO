21443448039de68674a3eecc9c3a763fbed8cce2
26007345 <maycom.maic@gmail.com>
2019-02-15 17:11:27 -0300

    Exercicio02

--- src/Exercico02/Volume.java
+++ src/Exercico02/Volume.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Volume {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Entre com a altura");
+        Double altura = sc.nextDouble();
+        System.out.println("Entre com o raio");
+        Double raio = sc.nextDouble();
+        Double volume = (3.141559 * Math.pow(raio,2) * altura);
+        System.out.println("Volume :"+ volume);
     }
 }
